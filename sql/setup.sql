6ce8bbec6ee17a848095542d75a22a26f04c328a
Richard <ricsmarsi@gmail.com>
2024-11-27 19:49:15 -0300

    Adding sys_user setup table (only to work with correct authentication models)

---
+++ sql/setup.sql
@@ -0,0 +1,20 @@
+# SYS USER DEFINITION
+
+-- drop table sys_user;
+-- drop sequence sys_user_seq;
+
+create sequence sys_user_seq start 1 cache 1 increment by 1;
+
+create table sys_user(
+    id_user integer,
+    username varchar(50),
+    email varchar(255),
+    password varchar(255),
+    created_at_ts timestamp,
+    createb_by integer,
+    change_password boolean,
+
+
+    constraint sys_user_pk primary key (id_user)    
+) tablespace sys_data_ts;
+
